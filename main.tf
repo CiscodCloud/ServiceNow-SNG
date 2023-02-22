00567c6b0d4f0cc96580bc567df688795123ea04
vivdalvi <36180626+vivdalvi@users.noreply.github.com>
2023-02-21 20:25:59 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,13 @@
 
+ terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.0"
+    }
+}
+}
+resource "random_integer" "test_random_int" {
+  min    = 0
+  max    = 100
+}
