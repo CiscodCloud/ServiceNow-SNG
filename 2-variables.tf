d95fb53cfe58a343e936bbdee92bdce38bf59656
vitberez <94049375+vitberez@users.noreply.github.com>
2023-07-25 12:27:50 +0200

    Update 2-variables.tf

--- 2-variables.tf
+++ 2-variables.tf
@@ -5,15 +5,12 @@ variable "apic_url" {
 
 variable "apic_username" {
   type = string
-  default = ""
 }
 
 variable "apic_password" {
   type = string
-  default = ""
 }
 
 variable "request_number" {
   type = string
-  default = ""
 }
